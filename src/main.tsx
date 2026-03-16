4d341d310c6dc30c962aefc360e7fd5c0f093b40
serhii.hubchakevych <serhii.hubchakevych@gmail.com>
2026-03-16 23:02:13 +0200

    Remove unused yaml dependency from package-lock.json and simplify rendering in main.tsx by removing StrictMode wrapper.

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 
 import App from './App'
@@ -6,7 +6,5 @@ import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />,
 )
